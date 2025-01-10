75dd1c9a80fde700a56312dd5221e56d1b1aca30
Gustavo <gustavopmenezes@usp.br>
2025-01-09 22:45:01 -0300

    refactor: comment out server configuration in vite.config.js so that the deploy works

--- vite.config.js
+++ vite.config.js
@@ -28,8 +28,8 @@ export default defineConfig({
     },
   },
 
-  server: {
-    host: '0.0.0.0', // Permite acesso via IP
-    port: 5173, // Porta padrão, você pode alterar se necessário
-  },
+  // server: {
+  //   host: '0.0.0.0', // Permite acesso via IP
+  //   port: 5173, // Porta padrão, você pode alterar se necessário
+  // },
 });
