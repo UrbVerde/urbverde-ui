7fbcdc5f0b38683f2c4a97bda1cd8f555df63ef7
Gustavo <gustavopmenezes@usp.br>
2025-01-27 11:08:38 -0300

    Update API endpoints to use full URLs and clean up unused imports

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,6 +1,6 @@
 // urbverde-ui/src/constants/endpoints.js
 export const API_URLS = {
-  SUGGESTIONS: '/v1/address/suggestions',
-  GEOCODE: '/v1/geocode',
-  CATEGORIES: '/v1/address/categories'
+  SUGGESTIONS: 'https://api.urbverde.com.br/v1/address/suggestions',
+  GEOCODE: 'https://api.urbverde.com.br/v1/geocode',
+  CATEGORIES: 'https://api.urbverde.com.br/v1/address/categories'
 };
