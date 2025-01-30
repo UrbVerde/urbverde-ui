600f4d256b1186f00cbfe88e658408c06f5a8080
vitorlacerda05 <vitorlacerda05@gmail.com>
2025-01-30 20:40:41 -0300

    feat(router): add smooth scroll in routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,18 @@ import AboutUs from '@/pages/AboutUs.vue';
 import WorkTogether from '@/pages/WorkTogether.vue';
 import ContactPage from '@/pages/ContactPage.vue';
 
-const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/mapa', name: 'MapPage', component: MapPage },
-  { path: '/sobre', name: 'AboutUs', component: AboutUs },
-  { path: '/parceiro', name: 'WorkTogether', component: WorkTogether },
-  { path: '/contato', name: 'ContactPage', component: ContactPage },
-];
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/mapa', name: 'MapPage', component: MapPage },
+    { path: '/sobre', name: 'AboutUs', component: AboutUs },
+    { path: '/parceiro', name: 'WorkTogether', component: WorkTogether },
+    { path: '/contato', name: 'ContactPage', component: ContactPage },
+  ],
+  scrollBehavior() {
+    return { top: 0, behavior: 'smooth' };
+  }
 });
 
 export default router;
