3937a061f5fef8d700a0994e09f2fd83eafb54c6
vitorlacerda05 <vitorlacerda05@gmail.com>
2025-01-31 00:42:10 -0300

    feat(contact): add InstitutionalContact component and integrate it into ContactPage

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -4,18 +4,43 @@
     page-title="Contatos"
     page-subtitle="Escolha a melhor forma para entrar em contato com a nossa equipe"
   >
-    aa
+    <InstitutionalContact
+      icon-name="bi-envelope"
+      title="E-mail"
+      description="Entre em contato pelo e-mail"
+      contact-info="comunica.urbverde@usp.br"
+      button-label="Enviar e-mail"
+      contact-link="mailto:comunica.urbverde@usp.br"
+    />
+    <InstitutionalContact
+      icon-name="bi-whatsapp"
+      title="WhatsApp"
+      description="Entre em contato pelo número"
+      contact-info="+5511916709802"
+      button-label="Enviar WhatsApp"
+      contact-link="http://wa.me/5511916709802"
+    />
+    <InstitutionalContact
+      icon-name="bi-chat-text"
+      title="Suporte"
+      description="Experimente usar o aplicativo de suporte
+        disponível no botão do canto direito inferior da tela"
+      :show-copy-button="false"
+      :show-button="false"
+    />
   </InstitutionalLayout>
 </template>
 
 <script>
 import InstitutionalLayout from '@/layouts/InstitutionialLayout.vue';
+import InstitutionalContact from '@/components/institutional_pages/InstitutionalContact.vue';
 import { useHead } from '@vueuse/head';
 
 export default {
   name: 'ContactPage',
   components: {
     InstitutionalLayout,
+    InstitutionalContact,
   },
 
   // Configuração das meta tags de SEO
@@ -26,12 +51,12 @@ export default {
         {
           name: 'description',
           content:
-                      'A UrbVerde é uma iniciativa que reúne mais de 50 pesquisadores de diversas instituições para democratizar o acesso a dados socioambientais e apoiar o planejamento urbano sustentável. Conheça nossa missão e impacto.',
+            'A UrbVerde é uma iniciativa que reúne mais de 50 pesquisadores de diversas instituições para democratizar o acesso a dados socioambientais e apoiar o planejamento urbano sustentável. Conheça nossa missão e impacto.',
         },
         {
           name: 'keywords',
           content:
-                      'UrbVerde, dados socioambientais, planejamento urbano, sustentabilidade, monitoramento ambiental, cidades verdes, acesso gratuito, pesquisa, smart cities, FAPESP, CNPq, imagens de satélite, gestão pública',
+            'UrbVerde, dados socioambientais, planejamento urbano, sustentabilidade, monitoramento ambiental, cidades verdes, acesso gratuito, pesquisa, smart cities, FAPESP, CNPq, imagens de satélite, gestão pública',
         },
         {
           property: 'og:title',
@@ -40,7 +65,7 @@ export default {
         {
           property: 'og:description',
           content:
-                      'Desde 2021, a UrbVerde tem sido referência em monitoramento ambiental urbano no Brasil, oferecendo dados gratuitos para apoiar políticas públicas e promover cidades mais resilientes.',
+            'Desde 2021, a UrbVerde tem sido referência em monitoramento ambiental urbano no Brasil, oferecendo dados gratuitos para apoiar políticas públicas e promover cidades mais resilientes.',
         },
       ],
     });
@@ -48,7 +73,15 @@ export default {
 };
 </script>
 
-  <style scoped lang="scss">
-      @import '@/assets/styles/breakpoints.scss';
-
-  </style>
+<style scoped lang="scss">
+.contacts-grid {
+  display: flex;
+  padding: 0px 24px;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+  gap: 32px;
+  align-self: stretch;
+  flex-wrap: wrap;
+}
+</style>
