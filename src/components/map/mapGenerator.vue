b220de623ab294159901f039a6fe64cb8f89b5cf
vitorlacerda05 <vitorlacerda05@gmail.com>
2025-04-12 12:39:49 -0300

    Update map settings: decrease initial and final zoom levels, and reduce animation duration for improved performance.

--- src/components/map/mapGenerator.vue
+++ src/components/map/mapGenerator.vue
@@ -100,9 +100,9 @@ let pinnedFeatureId = null;
 let hoveredSetorId = null;
 
 // Map constants
-const MAP_ZOOM_START = 14;
-const MAP_ZOOM_FINAL = 15;
-const MAP_ANIMATION_DURATION = 4000;
+const MAP_ZOOM_START = 13;
+const MAP_ZOOM_FINAL = 14;
+const MAP_ANIMATION_DURATION = 3000;
 
 // Get current layer id, scale and year from the URL query.
 const currentLayer = computed(() => route.query.layer);
