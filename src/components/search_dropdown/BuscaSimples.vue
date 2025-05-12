92c012ff3ed7219886eaf16a859032c8379e239e
elbuenogui <gbueno008@gmail.com>
2025-05-11 18:29:54 -0600

    Adição de novos modos de visualização 'map-mode' e 'policies-mode' no componente BuscaSimples.vue, com ajustes nos estilos de contorno para cada modo. Implementação de lógica para monitorar mudanças no modo de visualização a partir do store de localização.

--- src/components/search_dropdown/BuscaSimples.vue
+++ src/components/search_dropdown/BuscaSimples.vue
@@ -3,7 +3,13 @@
   <GetUserLocation @location-updated="updateLocationData" @location-error="handleLocationFailure" />
   <div class="search-wrapper">
     <div ref="inputContainer"
-         :class="{ 'input-container shadow-sm': !dropdown && !isError, 'input-container-dropdown shadow': dropdown && !isError, 'input-container-error shake-animation shadow': isError  }"
+         :class="{
+           'input-container shadow-sm': !dropdown && !isError,
+           'input-container-dropdown shadow': dropdown && !isError,
+           'input-container-error shake-animation shadow': isError,
+           'map-mode': viewMode === 'map',
+           'policies-mode': viewMode === 'policies'
+         }"
          @click="activateInput">
       <div class="input-overlay">
         <input ref="inputField"
@@ -158,6 +164,12 @@ const isError = ref(false);
 const codes = ref({});
 
 const locationStore = useLocationStore();
+
+const viewMode = ref(locationStore.viewMode);
+
+watch(() => locationStore.viewMode, (newMode) => {
+  viewMode.value = newMode;
+});
 
 // Watch para monitorar mudanças no store de localização
 watch(
@@ -860,10 +872,18 @@ function emitLocationUpdate(payload) {
 .input-container-dropdown {
   border-radius: 99px;
   background: var(--Gray-100, #F8F9FA);
-  outline: 3px solid #418377;
+  outline: 3px solid map-get($theme, primary);
   outline-offset: -3px;
   user-select: none;
   cursor: default;
+
+  &.map-mode {
+    outline: 3px solid map-get($theme, primary);
+  }
+
+  &.policies-mode {
+    outline: 3px solid map-get($yellow, 600);
+  }
 }
 
 /* Input and overlay */
@@ -1168,9 +1188,15 @@ function emitLocationUpdate(payload) {
   outline-offset: -3px;
 }
 
-  @include breakpoint-down('tablet') {
-    .input-container {
-      outline: 1px solid map-get($green, 500);
+@include breakpoint-down('tablet') {
+  .input-container {
+    &.map-mode {
+      outline: 1px solid map-get($theme, primary);
+    }
+
+    &.policies-mode {
+      outline: 1px solid map-get($yellow, 600);
     }
   }
+}
 </style>
