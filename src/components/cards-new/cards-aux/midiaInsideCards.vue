0d7557bb83cd1ef2f087348340b3b9b63eeec162
vitorlacerda05 <vitorlacerda05@gmail.com>
2025-05-13 15:29:31 -0300

    Add MidiaInsideCards and paragraphInsideCards components

---
+++ src/components/cards-new/cards-aux/midiaInsideCards.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="midia-wrapper">
+    <!-- Image handling -->
+    <img
+      v-if="typeMidia === 'image'"
+      :src="link"
+      :alt="'Imagem'"
+      class="midia-image"
+    />
+
+    <!-- Video handling -->
+    <div v-else-if="typeMidia === 'video'" class="video-container">
+      <div class="video-thumbnail" @click="openVideoModal">
+        <img
+          :src="getVideoThumbnail(link)"
+          alt="Video thumbnail"
+          class="video-thumbnail-img"
+        />
+        <div class="play-button">
+          <i class="fas fa-play"></i>
+        </div>
+      </div>
+    </div>
+
+    <!-- Video Modal -->
+    <div v-if="showVideoModal" class="video-modal" @click="closeVideoModal">
+      <div class="modal-content" @click.stop>
+        <button class="close-button" @click="closeVideoModal">&times;</button>
+        <iframe
+          :src="getEmbedUrl(link)"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MidiaInsideCards',
+  props: {
+    typeMidia: {
+      type: String,
+      required: true,
+      validator: (value) => ['image', 'video'].includes(value)
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      showVideoModal: false
+    };
+  },
+  methods: {
+    openVideoModal() {
+      this.showVideoModal = true;
+    },
+    closeVideoModal() {
+      this.showVideoModal = false;
+    },
+    getVideoThumbnail(url) {
+      // Extract video ID from YouTube URL
+      const videoId = this.extractVideoId(url);
+
+      return `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`;
+    },
+    getEmbedUrl(url) {
+      const videoId = this.extractVideoId(url);
+
+      return `https://www.youtube.com/embed/${videoId}?autoplay=1`;
+    },
+    extractVideoId(url) {
+      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+      const match = url.match(regExp);
+
+      return (match && match[2].length === 11) ? match[2] : null;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.midia-wrapper {
+  display: flex;
+  padding: 16px 24px;
+  align-items: center;
+  justify-content: center;
+}
+
+.midia-image {
+  max-width: 256px;
+  max-height: 256px;
+  width: auto;
+  height: auto;
+  object-fit: contain;
+}
+
+.video-container {
+  width: 320px;
+  height: 180px;
+  position: relative;
+  cursor: pointer;
+}
+
+.video-thumbnail {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+.video-thumbnail-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.play-button {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60px;
+  height: 60px;
+  background-color: rgba(0, 0, 0, 0.7);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 24px;
+}
+
+.video-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  position: relative;
+  width: 80%;
+  max-width: 800px;
+  background-color: black;
+}
+
+.modal-content iframe {
+  width: 100%;
+  height: 450px;
+}
+
+.close-button {
+  position: absolute;
+  top: -40px;
+  right: 0;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 30px;
+  cursor: pointer;
+  padding: 5px;
+}
+
+.close-button:hover {
+  color: #ccc;
+}
+</style>
---
+++ src/components/cards-new/cards-aux/paragraphInsideCards.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="card-paragraph">
+    <CardTitle
+      v-if="showCardTitle"
+      :title="title"
+      :subtitle="subtitle"
+      :show-subtitle="showSubtitle"
+      :show-left-icon="showLeftIcon"
+      :show-right-icon="showRightIcon"
+      :left-icon-class="leftIconClass"
+      :right-icon-class="rightIconClass"
+    />
+    <div class="text-wrapper">
+      <!-- Title Paragraph Wrapper -->
+      <div v-if="showParagraphTitle" class="titleParagraphWrapper">
+        <h5 class="heading-h5" :class="{ 'text-center': type === 'paragraph-center' }">
+          {{ paragraphTitle }}
+        </h5>
+      </div>
+
+      <!-- Content -->
+      <div class="content">
+        <!-- List Type -->
+        <ul v-if="type === 'list'" class="benefits-card-list">
+          <li v-for="(item, index) in listItems" :key="index" class="benefits-card-item">
+            <span class="benefits-card-number body-small-medium">{{ index + 1 }}</span>
+            <p class="body-small-regular" v-html="item"></p>
+          </li>
+        </ul>
+
+        <!-- Paragraph Left Type -->
+        <div v-else-if="type === 'paragraph-left'" class="paragraph-wrapper">
+          <p class="paragraph body-small-regular">{{ paragraphText }}</p>
+        </div>
+
+        <!-- Paragraph Center Type -->
+        <div v-else-if="type === 'paragraph-center'" class="paragraph-wrapper">
+          <p class="paragraph body-small-regular text-center">{{ paragraphText }}</p>
+        </div>
+
+        <!-- Slot Type -->
+        <slot v-else></slot>
+      </div>
+
+      <!-- Button Wrapper -->
+      <div v-if="showPrimaryButton" class="button-wrapper" :class="{ 'text-center': type === 'paragraph-center' }">
+        <PrimaryButton
+          :label="buttonLabel"
+          :filled="true"
+          :icon="buttonIcon"
+          :icon-type="buttonIconType"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CardTitle from './cardTitle.vue';
+import PrimaryButton from '@/components/buttons/PrimaryButton.vue';
+
+// CardTitle props
+defineProps({
+  // CardTitle props
+  showCardTitle: {
+    type: Boolean,
+    default: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  subtitle: {
+    type: String,
+    default: ''
+  },
+  showSubtitle: {
+    type: Boolean,
+    default: false
+  },
+  showLeftIcon: {
+    type: Boolean,
+    default: false
+  },
+  showRightIcon: {
+    type: Boolean,
+    default: false
+  },
+  leftIconClass: {
+    type: String,
+    default: 'bi bi-stars'
+  },
+  rightIconClass: {
+    type: String,
+    default: 'bi bi-stars'
+  },
+
+  // Paragraph Title props
+  showParagraphTitle: {
+    type: Boolean,
+    default: false
+  },
+  paragraphTitle: {
+    type: String,
+    default: ''
+  },
+
+  // Content type and content props
+  type: {
+    type: String,
+    default: 'list',
+    validator: (value) => ['list', 'paragraph-left', 'paragraph-center', 'slot'].includes(value)
+  },
+  listItems: {
+    type: Array,
+    default: () => []
+  },
+  paragraphText: {
+    type: String,
+    default: ''
+  },
+
+  // Button props
+  showPrimaryButton: {
+    type: Boolean,
+    default: false
+  },
+  buttonLabel: {
+    type: String,
+    default: ''
+  },
+  buttonIcon: {
+    type: String,
+    default: null
+  },
+  buttonIconType: {
+    type: String,
+    default: 'bootstrap'
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.card-paragraph {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.text-wrapper {
+  display: flex;
+  padding-bottom: 24px;
+  flex-direction: column;
+  align-items: flex-start;
+  align-self: stretch;
+}
+
+.titleParagraphWrapper {
+  display: flex;
+  padding: 24px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+
+  h5 {
+    color: map-get($body-text, body-color);
+    text-align: left;
+    margin: 0;
+
+    &.text-center {
+      text-align: center;
+    }
+  }
+}
+
+.paragraph-wrapper {
+  display: flex;
+  padding: 0px 24px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  align-self: stretch;
+
+  .paragraph {
+    color: map-get($body-text, body-color);
+    text-align: left;
+    margin: 0;
+
+    &.text-center {
+      text-align: center;
+    }
+  }
+}
+
+.button-wrapper {
+  display: flex;
+  padding: 24px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+
+  &.text-center {
+    align-items: center;
+  }
+}
+
+// List styles from TipsCard
+.benefits-card-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+
+.benefits-card-item {
+  display: flex;
+  align-items: flex-start;
+  gap: 12px;
+  padding: 0 24px;
+  margin-bottom: 16px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.benefits-card-number {
+  display: flex;
+  width: 24px;
+  height: 24px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  background-color: map-get($gray, 100);
+  color: map-get($body-text, body-color);
+}
+
+.benefits-card-item p {
+  margin: 0;
+  flex: 1;
+}
+</style>
